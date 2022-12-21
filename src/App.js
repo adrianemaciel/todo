af8465e5e8c415dfcf74d01029afaf3969d57a95
Adriane <adrianemaciel.developer@gmail.com>
2022-12-21 18:17:19 -0300

    wip: form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    // não recarrega a página ao enviar o formulário permitindo o fluxo SPA.
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      time,
+      title,
+      done: false,
+    };
+    // envio para a api
+    console.log(todo);
+
+    setTitle("");
+    setTime("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1> React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">O que vocÊ vai fazer?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Título da tarefa"
+              //setTitle: altera o estato do titulo e: evento; target: input; value: valor do input.
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="time">Duração:</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="Tempo estimado (em horas)"
+              //setTitle: altera o estato do titulo e: evento; target: input; value: valor do input.
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+
+          <input type="submit" value="Criar tarefas" />
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+      </div>
     </div>
   );
 }
